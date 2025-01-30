0ec75d811baea00c030f266db52efe9f99615981
vishnubansaltarento <vishnu.bansal@tarento.com>
2025-01-30 12:42:10 +0530

    update for jest config issue with angular 16

--- jest.config.js
+++ jest.config.js
@@ -17,24 +17,36 @@
 // }
 
 
+// module.exports = {
+//   preset: 'ts-jest',
+//   globals: {
+//     'ts-jest': {
+//        tsconfig: '<rootDir>/tsconfig.spec.json',
+//        stringifyContentPathRegex: '\\.(html|svg)$',
+//      },
+//    },
+//    coverageDirectory: './coverage',
+//    transform: {
+//     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+//   },
+//   transformIgnorePatterns: [
+//     'node_modules/(?!.*\\.mjs$)',
+//   ],
+//   coverageReporters: ["clover", "json", "lcov", "text", "text-summary"],
+//   collectCoverage: true,
+//   testResultsProcessor: "jest-sonar-reporter"
+
+
+// }
+
 module.exports = {
-  preset: 'ts-jest',
-  globals: {
-    'ts-jest': {
-       tsconfig: '<rootDir>/tsconfig.spec.json',
-       stringifyContentPathRegex: '\\.(html|svg)$',
-     },
-   },
-   coverageDirectory: './coverage',
-   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
-  },
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
   transformIgnorePatterns: [
     'node_modules/(?!.*\\.mjs$)',
   ],
   coverageReporters: ["clover", "json", "lcov", "text", "text-summary"],
   collectCoverage: true,
   testResultsProcessor: "jest-sonar-reporter"
-
-
-}
+};
