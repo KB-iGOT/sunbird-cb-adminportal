b1ca9cf6686a40dd323f51e110a6f4b9a9811177
Afzal84 <mohd.afzal@tarento.com>
2025-05-14 11:32:40 +0530

    removed the AM PM logic

--- project/ws/app/src/lib/routes/home/routes/events/create-event/create-event.component.ts
+++ project/ws/app/src/lib/routes/home/routes/events/create-event/create-event.component.ts
@@ -377,11 +377,8 @@ export class CreateEventComponent implements OnInit, OnDestroy {
   }
 
   convertToMinutes(timeStr: string): number {
-    const [time, modifier] = timeStr?.split(' ')
+    const [time] = timeStr?.split(' ')
     let [hours, minutes] = time?.split(':').map(Number)
-
-    if (modifier === 'PM' && hours !== 12) hours += 12
-    if (modifier === 'AM' && hours === 12) hours = 0
 
     return hours * 60 + minutes
   }
